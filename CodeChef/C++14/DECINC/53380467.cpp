9fa791265f10098d0bf5c87ff2843fc744c728da
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:40:33 +0530

    Committing files

---
+++ CodeChef/C++14/DECINC/53380467.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	    int t;
+	    cin >> t;
+	    if(t%4==0) cout << (t+1) ;
+	    else cout << (t-1);
+	    /* while(t--)
+	    {
+	       int a,b;
+    	       cin >> a >> b;
+    	       if(a>b) cout << a << " ";
+    	       else if (b>a) cout << b << " ";
+    	       cout << a+b << endl;
+	       
+	       
+	    }*/
+	  
+	    
+	return 0;
+}
