5ec5b5e93d98f1a27a3b69b4098c19808cd67f2e
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:59:54 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1447A | Add Candies/139975849.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif  
+ 
+    int T;
+    cin >> T;
+    while(T--)
+    { 
+        int n;
+        cin >> n;
+        cout << n << endl;
+        for(int i=0;i<n;i++)
+        {
+            cout << i+1 << " " ;
+        }
+        cout << endl;
+ 
+    }
+}
+ 
+       
+ 
+ 
+ 
