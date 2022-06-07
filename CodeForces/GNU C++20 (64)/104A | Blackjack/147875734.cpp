fc3076ac95a137d6322292fc66b950a13730eb06
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:50:22 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/104A | Blackjack/147875734.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+ 
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("outputter.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll n;
+    cin >> n;
+    if(n>=0 && n<=10) cout << 0 << endl;
+    if(n==11) cout << 4 << endl;
+    if(n>=12 && n<=19) cout <<4 << endl;
+    if(n==20) cout << 15 << endl;
+    if(n==21) cout << 4 << endl;
+    if(n>=22 && n<=25) cout << 0 << endl;
+ 
+    
+}
