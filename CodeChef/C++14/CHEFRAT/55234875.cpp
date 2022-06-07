676782f77c70f41b7fc634b3f14119c0b0cdd844
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:38:24 +0530

    Committing files

---
+++ CodeChef/C++14/CHEFRAT/55234875.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+
+    int p,n;
+     cin >> p >> n;
+     int a[n];
+     for(int i=0;i<n;i++)
+     {
+        cin >> a[i];
+        p=p+a[i];
+     }
+     cout << p ;
+
+}
+
+
+
+
