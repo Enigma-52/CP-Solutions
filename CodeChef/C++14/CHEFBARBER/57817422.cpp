733f4d150cbc708c812322e00bb7440f92050e08
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:33:27 +0530

    Committing files

---
+++ CodeChef/C++14/CHEFBARBER/57817422.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+int main()
+{
+ 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,m;
+        cin >> n >> m;
+        cout << n*m << endl;
+    }
+
+        
+
+
+ 
+ 
+    
+ 
+}
