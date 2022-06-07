f33aa1b06661303ca89e9bdf88b772b3c2a9ba20
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:40:05 +0530

    Committing files

---
+++ CodeChef/C++14/SLOOP/53821666.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+       int m,s;
+       cin >> m >> s;
+       int n=m/s;
+       cout << n << endl;
+    }
+}
