c687f7a81aa005a5d057ea902045ef080d218f13
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:54:56 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1335A | Candies and Two Sisters/143247401.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        cout << (n-1)/2 << endl;
+ 
+    }  
+            
+        
+ 
+ 
+ 
+}                   
