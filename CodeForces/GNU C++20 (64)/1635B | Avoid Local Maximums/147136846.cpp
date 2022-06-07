488b4c7dc4780571d66b13524d50f627e5b8c0a4
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:50:58 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1635B | Avoid Local Maximums/147136846.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin >> a[i];
+        }
+        std::vector<ll> v;
+        ll cnt=0;
+        ll ind=-1;
+        for(int i=1;i<n-1;i++)
+        {   
+            if(a[i]>a[i-1] && a[i]>a[i+1] && i+2<n)
+            {
+                a[i+1]=max(a[i],a[i+2]);
+                cnt++;
+            }
+            if(a[i]>a[i-1] && a[i]>a[i+1] )
+            {
+                a[i]=a[i-1];
+                cnt++;
+            }
+ 
+        }
+        
+        cout << cnt << endl;
+        for(int i=0;i<n;i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+ 
+ 
+ 
+    }
+ 
+}
