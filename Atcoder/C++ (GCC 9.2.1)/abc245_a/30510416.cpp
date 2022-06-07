a9da3b99020dc4b6eed07a2c73e0b4c4cbeb0930
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:31:39 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc245_a/30510416.cpp
@@ -0,0 +1,143 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+
+// ll t;
+// cin >> t;  
+//  for(int i=1;i<=t;i++)
+//  {
+//     cout << "Case #" << i << ": " ;
+//  }
+
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+ll getSum(ll n)
+    {
+        ll sum = 0;
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+void fillPrefixSum(ll arr[], ll n, ll prefixSum[]) 
+{ 
+    prefixSum[0] = arr[0]; 
+
+    // Adding present element 
+    // with previous element 
+    for (ll i = 1; i < n; i++) 
+        prefixSum[i] = prefixSum[i-1] + arr[i]; 
+} 
+
+bool isFactorial(long long int n)
+{
+  for (long long int i = 1;; i++) {
+    if (n % i == 0) {
+      n /= i;
+    }
+    else {
+      break;
+    }
+  }
+ 
+  if (n == 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+ll power(ll x, ll y)
+{
+    int temp;
+    if( y == 0)
+        return 1;
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return temp * temp;
+    else
+        return x * temp * temp;
+}
+ 
+bool isPrime(ll n) {
+    for (int i = 2; i*i <= n; ++i) {
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("outtt.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll a,b,c,d;
+    cin >> a >> b >> c >> d;
+    if(a<c)
+    {
+        cout << "Takahashi" << endl;
+        return 0;
+    }
+    if(a==c)
+    {
+        if(b<d)
+        {
+            cout << "Takahashi" << endl;
+        return 0;
+        }
+        if(b==d)
+        {
+            cout << "Takahashi" << endl;
+        return 0;
+        }
+        cout << "Aoki" << endl;
+        return 0;
+    }
+cout << "Aoki" << endl;
+        return 0;
+}
