13a9c84cf1d2fa2bccb32f06a53754bc1d9172f4
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:37:56 +0530

    Committing files

---
+++ CodeChef/C++14/FRIMEET/56868645.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+
+int main()
+{
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll x1,x2;
+        cin >> x1 >> x2;
+        if(x1>=x2) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+
+    }
+
+ 
+ 
+    
+}
+
+
+
+
+
+
+        
+      
