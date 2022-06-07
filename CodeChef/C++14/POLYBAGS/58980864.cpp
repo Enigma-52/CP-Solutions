733f1260834b5a96a77349778a73ed23b25cc8f4
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:33:06 +0530

    Committing files

---
+++ CodeChef/C++14/POLYBAGS/58980864.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+
+int main()
+{
+ 
+   
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+    while(t--)
+    { 
+       ll n;
+       cin >> n;
+       ll cnt=0;
+       if(n%10==0) cnt=n/10;
+       else cnt=(n/10)+1;
+       cout << cnt << endl;
+
+    }
+}
