f87dbdfb836ecf5b51b2c4b159a2fdda9085303e
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:37:57 +0530

    Committing files

---
+++ CodeChef/C++14/TWOROOKS/56868240.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+
+int main()
+{
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll x1,y1,x2,y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        if(x1==x2 || y1==y2) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+    }
+
+ 
+ 
+    
+}
+
+
+
+
+
+
+        
+      
