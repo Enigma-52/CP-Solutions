389a348ca8f1a2e397ad722bd0d5886db1146e42
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:34:55 +0530

    Committing files

---
+++ CodeChef/C++14/ADDNATRL/54271209.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    long long  s=0;
+    for(int i=1;i<=n;i++)
+    {
+        s=s+i;
+    }
+    cout << s;
+
+     
+}
