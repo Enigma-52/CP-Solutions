a3a5f00e68bbf82a424443c676e031616ae9e15c
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:38:11 +0530

    Committing files

---
+++ CodeChef/C++14/ELECTIONS/56735136.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+
+
+int main()
+{
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll a,b,c;
+        cin >> a >> b >> c;
+        if(a>50)
+        {
+            cout << "A" << endl;
+            continue;
+        }
+        if(b>50)
+        {
+            cout << "B" << endl;
+            continue;
+        }
+        if(c>50)
+        {
+            cout << "C" << endl;
+            continue;
+        }
+        else
+        {
+            cout << "NOTA" << endl;
+            continue;
+        }
+                
+
+
+
+    }
+        }
+    
+            
+        
+
+
+
+                
