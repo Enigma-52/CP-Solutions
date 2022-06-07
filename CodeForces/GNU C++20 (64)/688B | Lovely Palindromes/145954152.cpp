b12a99b5e9991d546cae6a3d474e2e1c01ef8706
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:52:20 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/688B | Lovely Palindromes/145954152.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    string s;
+    cin >> s;
+    string k=s;
+    reverse(k.begin(), k.end());
+    cout << s << k << endl;
+ 
+        
+ 
+       
+ 
+ 
+    
+}
