dbea9e35c480a06f537669d04fe7513e0e68243b
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:39:12 +0530

    Committing files

---
+++ CodeChef/C++14/CHFRICH/54302120.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b,x;
+        cin >> a >> b >> x;
+        a=b-a;
+        a=a/x;
+        cout << a << endl;
+        
+    }
+    
+}
