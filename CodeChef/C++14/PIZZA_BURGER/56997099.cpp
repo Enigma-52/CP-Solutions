66c9d813684f6e2ba5091a6496dad744decc47ce
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:37:54 +0530

    Committing files

---
+++ CodeChef/C++14/PIZZA_BURGER/56997099.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+
+int main()
+{
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+       
+       ll x,y,z;
+       cin >> x >> y >> z;
+
+if(x>=y) cout << "PIZZA" << endl;
+else if(x>=z) cout << "BURGER" << endl;
+else cout << "NOTHING" << endl;
+       
+
+
+    }
+
+ 
+ }
+    
