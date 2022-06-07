45a73d482bf454a38d286ce79207c7d2d595036a
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:53:45 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1632A | ABC/144609463.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+   ll t;
+   cin >> t;
+   while(t--)
+   {
+        
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(n>2 || s=="11" || s=="00")
+    {
+        cout << "NO" << endl;
+    }
+    else cout << "YES" << endl;
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+}
+ 
+ 
