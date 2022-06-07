cdb9e01b7dc37e0afb292b5ebf15e3759ababfbf
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:59:44 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1619B | Squares and Cubes/140042912.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif  
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+      long long n;
+      cin >> n;
+      long long cnt=0;
+    long long res = (long long)sqrt(n) + (long long)cbrt(n)
+              - (long long)(sqrt(cbrt(n)));
+              cout << res << endl;
+ 
+        
+        
+     }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
