187055682da1ab3fc01935adafeeb5184cca40f9
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:37:16 +0530

    Committing files

---
+++ CodeChef/C++14/SUBPERM/59228328.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+ll getSum(ll n)
+    {
+        ll sum = 0;
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+int main()
+{
+ 
+    
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,k;
+        cin >> n >> k;
+        if(k==1 && n>1)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+        std::vector<ll> v;
+        for(int i=0;i<n;i++)
+        {
+            v.pb(i+1);
+        }
+        reverse(v.end()-(n-k+1),v.end());
+        for(auto x:v)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }  
+}
