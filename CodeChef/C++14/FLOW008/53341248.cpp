8bc5d2b30b0bae2481977f66a113752faf00ec63
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:40:40 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW008/53341248.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	    int t;
+	    cin >> t;
+	    int s1=0,s2=0;
+	    int p=0,q=0;
+	    while(t--)
+	    {
+	       int n;
+	       cin >> n;
+	       if(n<10) cout << "Thanks for helping Chef!" << endl;
+	       else cout << "-1" << endl;
+	    }
+	  
+	    
+	return 0;
+}
