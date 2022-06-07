fee50831ac3752cf13a99ab76e5bf78c14fd141e
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:33:19 +0530

    Committing files

---
+++ CodeChef/C++14/TRAVELFAST/58533601.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+ 
+int main()
+{
+ 
+   
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+
+        ll x,y;
+        cin >> x >> y;
+        if(x<y)
+        {
+            cout << "BIKE" << endl;continue;
+        }
+        if(y<x)
+        {
+            cout << "CAR" << endl;continue;
+        }
+        if(x==y)
+        {
+            cout << "SAME" << endl;continue;
+        }
+
+
+    }
+    
+
+
+   
+
+
+   
+}
