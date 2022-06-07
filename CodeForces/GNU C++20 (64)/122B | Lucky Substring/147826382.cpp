7ad33040ef4c417a28a50f4330a9e02f9c50cd5f
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:50:37 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/122B | Lucky Substring/147826382.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+ 
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("outputter.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+    
+       string s;
+       cin >> s;
+       int n=s.length();
+       ll four=0,seven=0;
+       for(int i=0;i<n;i++)
+       {
+            if(s[i]=='4') four++;
+            if(s[i]=='7')seven++;
+       }
+       if(max(four,seven)==0)
+       {
+        cout << -1 << endl;
+        return 0;
+       }
+       if(four==seven)
+       {
+         cout << 4 << endl;
+         return 0;
+       }
+       if(four>seven)
+       {
+        cout << 4 << endl;
+         return 0;
+       }
+       if(four<seven)
+       {
+        cout << 7 << endl;
+         return 0;
+       }
+ 
+ 
+ 
+    
+    
+}
