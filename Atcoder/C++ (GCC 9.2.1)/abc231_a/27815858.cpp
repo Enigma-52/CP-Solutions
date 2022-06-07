fb2f25dd4b8afa8dba9d7b33b66d8cfcf97df7d5
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:32:00 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc231_a/27815858.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif 
+
+    long long d;
+    cin >> d;
+    double x=d/100.0;
+    cout << x;
+
+}
+
+
+
+
+
+
+
+
+
+        
