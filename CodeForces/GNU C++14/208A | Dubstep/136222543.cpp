f0dadf48a7b5fa0218a3801666d6b24b4a5f08e9
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 15:02:06 +0530

    Committing files

---
+++ CodeForces/GNU C++14/208A | Dubstep/136222543.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+ 
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif  
+  string s;
+  cin >> s;
+  for(int i=0;i<s.length();i++)
+  {
+    if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B' && i+2<s.length())
+    {
+      cout << " ";
+      i=i+2;
+    }
+    else cout << s[i];
+  }  
+      
+}
