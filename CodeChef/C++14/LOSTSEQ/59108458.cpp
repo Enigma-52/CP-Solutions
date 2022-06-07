4948783d820a8394812908c4a7db5458a00ba582
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:37:18 +0530

    Committing files

---
+++ CodeChef/C++14/LOSTSEQ/59108458.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+
+
+int main()
+{
+ 
+    
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll sum=0;
+        ll a[2*n];
+        for(int i=0;i<2*n;i++)
+        {
+            cin >> a[i];
+            sum+=a[i];
+        }
+        if(sum%2==0)
+        {
+            cout << "YES" << endl;
+        }
+        else cout << "NO" << endl;
+
+    }
+    
+}
