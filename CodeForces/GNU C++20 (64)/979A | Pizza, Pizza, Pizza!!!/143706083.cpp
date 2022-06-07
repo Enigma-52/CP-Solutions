88e425b51a6e8819380b64a30535ae3bd84290cc
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:54:46 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/979A | Pizza, Pizza, Pizza!!!/143706083.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+void bubbleSort(int arr[], int n,int b[]) 
+{ 
+ 
+} 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll n;
+    cin >> n;
+    if(n==0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+    if(n%2==0)
+    {
+        cout << n+1;
+    }
+    else cout << (n+1)/2;
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+        
+      
