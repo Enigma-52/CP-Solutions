db0fe9738626fd953de391587ef2dffca387b8e5
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:54:54 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1335B | Construct the String/143247729.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        for (int i = 0; i < n; ++i) {
+            cout << char('a' + i % b);
+        }
+        cout << endl;
+    }  
+            
+        
+ 
+ 
+ 
+}                   
