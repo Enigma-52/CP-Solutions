05d67fff0c91594e750f754830d6946fc52a6249
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:41:02 +0530

    Committing files

---
+++ CodeChef/C++14/PASSORFAIL/53271244.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,x,p;
+	    cin >> n >> x>>p;
+	    if((x*3)-((n-x))>=p) cout << "PASS" << endl;
+	    else cout << "Fail" << endl;
+	}
+	return 0;
+}
