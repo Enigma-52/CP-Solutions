b929fb2371c6a3a98e00caa4031e399a36218a94
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:48:37 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1447B | Numbers Box/150529428.cpp
@@ -0,0 +1,168 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+// Google
+// ll t;
+// cin >> t;  
+//  for(int i=1;i<=t;i++)
+//  {
+//     cout << "Case #" << i << ": " ;
+//  }
+ 
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+ 
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+ll getSum(ll n)
+    {
+        ll sum = 0;
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+ 
+void fillPrefixSum(ll arr[], ll n, ll prefixSum[]) 
+{ 
+    prefixSum[0] = arr[0]; 
+ 
+    // Adding present element 
+    // with previous element 
+    for (ll i = 1; i < n; i++) 
+        prefixSum[i] = prefixSum[i-1] + arr[i]; 
+} 
+ 
+bool isFactorial(long long int n)
+{
+  for (long long int i = 1;; i++) {
+    if (n % i == 0) {
+      n /= i;
+    }
+    else {
+      break;
+    }
+  }
+ 
+  if (n == 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+ll power(ll x, ll y)
+{
+    int temp;
+    if( y == 0)
+        return 1;
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return temp * temp;
+    else
+        return x * temp * temp;
+}
+ 
+int palindrome(string s,int l,int r)
+{
+    int f=1;
+    while(l<r)
+    {
+        if(s[l]==s[r])
+        {
+            l++;
+            r--;
+        }
+        else {f=0;break;}
+    }
+    return f;
+}
+ 
+int good(string s)
+{
+    int c1=0,c2=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='(') c1++;
+        else c2++;
+    }
+    if(c1==c2) return 1;
+    else return 0;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("outputter.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,m;
+        cin >> n >> m;
+        ll a[n][m];
+        ll cnt=0;
+        ll min=0;
+        ll sum=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin >> a[i][j];
+                sum+=abs(a[i][j]);
+                if(a[i][j]<0) cnt++;
+                if(i==0 && j==0)
+                {
+                    min=abs(a[i][j]);
+                }
+                else if(abs(a[i][j])<min)
+                {
+                    min=abs(a[i][j]);
+                }
+            }
+        }
+        if(cnt%2==1)
+        {
+            sum=sum-(2*min);
+        }
+        cout << sum <<endl;
+    }
+ 
+ 
+ 
+     
+    
+}
