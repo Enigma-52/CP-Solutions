554d0e60816432df0e8f9903dde9716653307235
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:40:43 +0530

    Committing files

---
+++ CodeChef/C++14/FSQRT/53340489.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <iomanip> 
+#include <cmath>
+#include<string>
+using namespace std;
+
+int main()
+{
+	    int t;
+	    cin >> t;
+	    while(t--)
+	    {
+	        int n;
+	        cin >> n;
+	        int k=sqrt(n);
+	        cout << k << endl;
+	    }
+	    
+	return 0;
+}
