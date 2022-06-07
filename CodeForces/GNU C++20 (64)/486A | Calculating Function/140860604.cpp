27d2e5fe75a7cea3eebc3c2825fbcc7ffcdc6200
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:58:05 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/486A | Calculating Function/140860604.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+ 
+ 
+// 3
+// 2 828
+// 4 392
+// 4 903
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    
+    ll n;
+    cin >> n;
+    cout << n/2-n%2*n;
+ 
+}
