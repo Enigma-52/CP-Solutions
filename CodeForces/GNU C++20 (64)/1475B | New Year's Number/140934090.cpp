c3c7e12c67bacd8c3a13d570432181465801896d
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:57:47 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1475B | New Year's Number/140934090.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+    cin >> n;
+    int cnt2021 = n % 2020;
+    int cnt2020 = (n - cnt2021) / 2020 - cnt2021;
+    if (cnt2020 >= 0 && 2020 * cnt2020 + 2021 * cnt2021 == n) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+  }
+  
+ 
+}
