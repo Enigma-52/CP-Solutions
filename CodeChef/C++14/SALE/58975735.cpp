5bacba61a3523555e779c711a0a246e0496ef9c4
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:33:09 +0530

    Committing files

---
+++ CodeChef/C++14/SALE/58975735.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+
+int main()
+{
+ 
+    
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll t;
+    cin >> t;
+    while(t--)
+    { 
+       ll a,b,c;
+       cin >> a >> b >> c;
+       std::vector<ll> v{a,b,c};
+       sort(v.begin(), v.end());
+       cout << v[1]+v[2] << endl;
+
+    }
+}
