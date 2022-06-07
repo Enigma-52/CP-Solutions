c908706097e8a04492ed8a67fd4a3d27973e480d
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:40:02 +0530

    Committing files

---
+++ CodeChef/C++14/COCONUT/53847570.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a,b,X,Y;
+        cin >> a >> b >> X >> Y;
+        cout << ((X/a)+(Y/b)) << endl;
+        
+    }
+}
