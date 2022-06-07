8b5c43281753a7fabe2a9478461362125c833811
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:39:11 +0530

    Committing files

---
+++ CodeChef/C++14/RELATIVE/54302161.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long g,c;
+        cin >> g >> c;
+        long long y=c*c;
+        y=y/2;
+        y=y/g;
+        cout << y << endl;
+        
+    }
+    
+}
