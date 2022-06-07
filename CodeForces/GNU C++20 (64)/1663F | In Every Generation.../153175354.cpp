50b2fbdfa61f08a6f40e9518c59ebb13b1b5ab38
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:46:07 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1663F | In Every Generation.../153175354.cpp
@@ -0,0 +1,201 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+#define pb push_back
+#define fs first
+#define sec second
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+ 
+// sss
+// ll t;
+// cin >> t;  
+//  for(int tt=1;tt<=t;tt++)
+//  {
+//     cout << "Case #" << tt << ": " ;
+//  }
+ 
+int odd(int n)
+{
+    if(n%2!=0) return 1;
+    else return 0;
+}
+int even(int n)
+{
+    if(n%2==0) return 1;
+    else return 0;
+}
+ 
+long long gcd(long long  a, long long  b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+ll getSum(ll n)
+    {
+        ll sum = 0;
+        while (n != 0) {
+            sum = sum + n % 10;
+            n = n / 10;
+        }
+        return sum;
+    }
+ 
+void fillPrefixSum(ll arr[], ll n, ll prefixSum[]) 
+{ 
+    prefixSum[0] = arr[0]; 
+ 
+    // Adding present element 
+    // with previous element 
+    for (ll i = 1; i < n; i++) 
+        prefixSum[i] = prefixSum[i-1] + arr[i]; 
+} 
+ 
+bool isFactorial(long long int n)
+{
+  for (long long int i = 1;; i++) {
+    if (n % i == 0) {
+      n /= i;
+    }
+    else {
+      break;
+    }
+  }
+ 
+  if (n == 1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+ll power(ll x, ll y)
+{
+    int temp;
+    if( y == 0)
+        return 1;
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return temp * temp;
+    else
+        return x * temp * temp;
+}
+ 
+ bool ispalin(int n)
+{  int ans=0;
+int temp=n;
+    while(n>0)
+    {
+            int d=n%10;
+       ans=ans*10+d;
+        n=n/10;
+    }
+ 
+    if(ans==temp)
+        return true;
+    else
+        return false;
+}
+bool isPrime(int n) {
+    for (int i = 2; i*i <= n; ++i) {
+        if(n%i == 0) return false;
+    }
+    return true;
+}
+ 
+unsigned int factorial(unsigned int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1)%998244353;
+}
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("outtt.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin >> s;
+    ll n=s.length();
+    string ss="";
+    if(n==3)
+    {
+        string k="the";
+        for(int i=0;i<3;i++)
+        {
+            int c1=s[i]-97;
+            int c2=k[i]-97;
+            c1=(c1+c2)%26;
+            c1+=97;
+            ss+=(char)c1;
+        }
+    }
+    if(n==5)
+    {
+        string k="buffy";
+        for(int i=0;i<5;i++)
+        {
+            int c1=s[i]-97;
+            int c2=k[i]-97;
+            c1=(c1+c2)%26;
+            c1+=97;
+            ss+=(char)c1;
+        }    }
+    if(n==6)
+    {
+        string k="slayer";
+        for(int i=0;i<6;i++)
+        {
+            int c1=s[i]-97;
+            int c2=k[i]-97;
+            c1=(c1+c2)%26;
+            c1+=97;
+            ss+=(char)c1;
+        }
+    }
+    if(n==7)
+    {
+        string k="vampire";
+        for(int i=0;i<7;i++)
+        {
+            int c1=s[i]-97;
+            int c2=k[i]-97;
+            c1=(c1+c2)%26;
+            c1+=97;
+            ss+=(char)c1;
+        }
+    }
+    if(n!=3 && n!=5 && n!=6 && n!=7)
+    {
+        cout << "none" << endl;
+    }
+    else cout << ss << endl;
+    
+ 
+    
+ 
+}
