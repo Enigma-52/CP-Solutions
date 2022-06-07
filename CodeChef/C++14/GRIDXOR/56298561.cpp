3c87d185b0af4b7019f57953228b913a9a0ff74b
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-07 14:33:54 +0530

    Committing files

---
+++ CodeChef/C++14/GRIDXOR/56298561.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+
+
+
+int main()
+{
+
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout << 1 << " ";
+            }
+            cout << endl;
+        }
+
+    }
+}
+ 
+ 
