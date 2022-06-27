fd4c0c22a5deaf499f56d4d5f2b5e229a9fa5f3b
Rohit Singh <95529619+Enigma-52@users.noreply.github.com>
2022-06-27 14:04:57 +0530

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1559A | Mocha and Math/143237029.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define endl '\n'
+#define debug(x) cout << x << endl;
+#define ll long long
+#define mod 1000000007
+ 
+//fast factors
+//count distinct array
+// fast prime
+// XOR(num & string)
+ 
+ 
+int main()
+{
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("IN.txt","r",stdin);
+    freopen("OUT.txt","w",stdout);
+    #endif 
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll a[n];
+        for(ll i=0;i<n;i++)
+        {
+            cin >> a[i];
+            a[0]=a[0]&a[i];
+        }
+        cout << a[0] << endl;
+ 
+ 
+    }
+        
+    
+ 
+ 
+ 
+}
